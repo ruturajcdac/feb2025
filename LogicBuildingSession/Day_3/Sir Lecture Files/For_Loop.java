2b8c643e5a28714bea0af205c310b9c913229424
ruturajcdac <rsa.cdac.kh@gmail.com>
2025-02-23 21:41:53 +0530

    Add files via upload

---
+++ LogicBuildingSession/Day_3/Sir Lecture Files/For_Loop.java
@@ -0,0 +1,35 @@
+class ForLoop{
+	public static void main(String args[]){
+	
+	/*
+	int a = 10;
+	for(; a>=5 ; System.out.println("Hellllo")){
+		System.out.println(a);
+		a--;
+	}
+	*/
+	
+	
+	/* infinite loop
+	for(; ;){
+		System.out.println("a");
+	}
+	*/
+
+	/* unreachable error
+	for(; ;){
+		System.out.println(10);
+	}
+	System.out.println("ABC");   // not reachable
+	*/
+
+	
+
+
+	}		
+
+
+
+
+
+}
